89c38d87b5020d0946fd17c0d75482d0e819d0e2
Mekylei-Belchior <mekylei@msn.com>
2020-07-02 17:12:59 -0300

    Update __init__.py

--- combinacoes/__init__.py
+++ combinacoes/__init__.py
@@ -1,1 +1,6 @@
-__all__ = ['possibilidades', 'resultados', 'analises']
+__all__ = ['analises', 
+		   'gerar_combinacoes',
+		   'indice_resultado',
+		   'possibilidades',
+		   'resultados'
+		   ]
